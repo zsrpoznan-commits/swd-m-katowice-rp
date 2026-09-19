ed3a968f905bec383e6ba9c76be2ae134d7f2dde
zsrpoznan-commits <zsrpoznan@gmail.com>
2026-09-19 14:54:00 +0100

    Add files via upload

---
+++ konta-funkcjonariuszy.js
@@ -0,0 +1,36 @@
+// KONTA FUNKCJONARIUSZY RP — TYLKO FIKCYJNE DANE
+window.RP_ACCOUNTS = [
+  {
+    id: "ADMIN-001",
+    name: "Administrator RP",
+    rank: "administrator",
+    dept: "WRD",
+    call: "MO-102",
+    role: "administrator",
+    password: "demo",
+    pin: "1234",
+    active: true
+  },
+  {
+    id: "WRD-204",
+    name: "Jan Kowalski",
+    rank: "sierż.",
+    dept: "WRD",
+    call: "MO-204",
+    role: "administrator",
+    password: "haslo123",
+    pin: "2040",
+    active: true
+  },
+  {
+    id: "WRD-205",
+    name: "Seba",
+    rank: "sierż.",
+    dept: "WRD",
+    call: "MO-205",
+    role: "administrator",
+    password: "krępa123",
+    pin: "2040",
+    active: true
+  }
+];
