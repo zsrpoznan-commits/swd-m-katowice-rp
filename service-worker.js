6567520f377ac3d780d7bc0e8c3d6bc8b8c8e17b
zsrpoznan-commits <zsrpoznan@gmail.com>
2026-09-19 21:58:58 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='swdm-katowice-rp-v42.0.0';
+const ASSETS=['./','./index.html','./style.css','./app.js','./konta-funkcjonariuszy.js','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
