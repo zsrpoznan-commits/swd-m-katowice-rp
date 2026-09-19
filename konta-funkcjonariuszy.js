a1a1ffc10eee0416dfd1a8eefbf058986d7b0c35
zsrpoznan-commits <zsrpoznan@gmail.com>
2026-09-19 20:43:37 +0100

    Update konta-funkcjonariuszy.js

--- konta-funkcjonariuszy.js
+++ konta-funkcjonariuszy.js
@@ -3,6 +3,6 @@
 window.RP_ACCOUNTS = [
   {id:'ADMIN-001',name:'Administrator RP',rank:'administrator',dept:'WRD',call:'MO-102',role:'administrator',password:'demo',pin:'1234',active:true},
   {id:'WRD-204',name:'Jan Kowalski',rank:'sierż.',dept:'WRD',call:'MO-204',role:'administrator',password:'haslo123',pin:'2040',active:true},
-  {id:'WRD-205',name:'Seba',rank:'sierż.',dept:'WRD',call:'MO-205',role:'administrator',password:'krępa123',pin:'2040',active:true}
+  {id:'WRD-205',name:'Seba',rank:'sierż.',dept:'WRD',call:'MO-205',role:'administrator',password:'krępa123',pin:'2040',active:true},
   {id:'WRD-206',name:'Piotr',rank:'insp.',dept:'WRD',call:'MO-206',role:'administrator',password:'Wika',pin:'2137',active:true}
 ];
